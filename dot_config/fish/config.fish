b662d8bf1ce3a5005c8af1947c6744626d4a66b2
Moritz Reiter <mail@moritzreiter.de>
2023-09-22 09:32:15 +0200

    switch from exa to eza

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -33,14 +33,16 @@ if begin set -q TERM; and command --quiet --search fortune; end
   fortune oblique
 end
 
-if command --quiet --search exa
-  alias ll="exa -l -g --icons --git"
-  alias la="exa -la -g --icons --git"
-  alias llt="exa -1 --icons --tree --git-ignore"
+if command --quiet --search eza
+  alias ll="eza -l -g --icons --git"
+  alias la="eza -la -g --icons --git"
+  alias llt="eza -1 --icons --tree --git-ignore"
 end
 
 # Starship prompt
 if command --quiet --search starship
   starship init fish | source
 end
+
+zoxide init fish | source
 
