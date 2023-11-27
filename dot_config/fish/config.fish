6963718df74f2eace12f7a598a3142025df24155
Moritz Reiter <mail@moritzreiter.de>
2023-11-27 14:21:41 +0100

    streamline fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,31 +1,32 @@
+# tmux please, but only if not in JetBrains terminal
 if status is-interactive
 and not set -q TMUX
 and [ "$TERMINAL_EMULATOR" != "JetBrains-JediTerm" ]
   exec tmux new-session -A -s default
 end
 
+# fish
 set -g fish_key_bindings fish_hybrid_key_bindings
+set fish_greeting ""
 
 set -x EDITOR vim
 set -x UNCRUSTIFY_CONFIG ~/.config/uncrustify
+set -x PYTHONSTARTUP ~/.config/python
 
-# Python
-set -x PYTHONSTARTUP ~/.pythonrc
-set -x PIP_REQUIRE_VIRTUALENV true
-set -x VIRTUAL_ENV_DISABLE_PROMPT true
 
-# Show colors in `less`
+# show colors in `less`
 set -x LESS -r
 
 alias svim='sudo -E vim'
 
 set workDir (dirname (status -f))
 
-# Load macOS specific stuff
+# Load macOS specific config
 if test (uname) = Darwin
   source $workDir"/config.macos.fish"
 end
 
+# load host specific config
 if begin
   test (hostname) = golem-pi3
   or test (hostname) = golem-pi-zero
@@ -33,19 +34,19 @@ end
   source $workDir"/config.ssh-keychain.fish"
 end
 
-set fish_greeting ""
-
-if begin set -q TERM; and command --quiet --search fortune; end
-  fortune oblique
-end
-
+# nicer file listings with eza
 if command --quiet --search eza
   alias ll="eza -l -g --icons --git"
   alias la="eza -la -g --icons --git"
   alias llt="eza -1 --icons --tree --git-ignore"
 end
 
-# Starship prompt
+# tell a fortune
+if begin set -q TERM; and command --quiet --search fortune; end
+  fortune oblique
+end
+
+# launch starship
 if command --quiet --search starship
   starship init fish | source
 end
