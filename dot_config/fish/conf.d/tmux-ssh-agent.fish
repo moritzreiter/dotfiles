fee6d6f82b425f81ca0ac10480ed69892c63ef4a
Moritz Reiter <mail@moritzreiter.de>
2026-05-16 08:13:44 +0000

    Remove dysfunctional fish tmux ssh config

--- dot_config/fish/conf.d/tmux-ssh-agent.fish
+++
@@ -1,7 +0,0 @@
-if command -q tmux; and set -q TMUX
-    set -l sock \
-        (tmux show-environment SSH_AUTH_SOCK 2>/dev/null)
-
-    set -gx SSH_AUTH_SOCK \
-        (string replace 'SSH_AUTH_SOCK=' '' -- $sock)
-end
